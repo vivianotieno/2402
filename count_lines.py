bd0480ed0a40e1f819e78072892ca221f1f00fb8
VivianOtieno <33otienovivy@gmail.com>
2024-02-08 15:27:49 +0100

    count_lines.py: add output description in the docstrings

--- count_lines.py
+++ count_lines.py
@@ -1,7 +1,13 @@
+"""
+Aim: this script counts the number of lines in standard input
+Input: STrings from the command line
+OUtput: 
+"""
+
 import sys
 
 count = o
 for line in  sys.std:
-	count =+
-
+	count =+ 1
+print(count, "lines in standard input")
 
